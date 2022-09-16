87e2b5b17c67d0177df1a8b49f677af658229a1c
matancohen <matan.cohen9895@gmail.com>
2022-09-16 10:18:05 +0300

    configure mongo DB

--- MongoDB.py
+++ MongoDB.py
@@ -1,3 +1,13 @@
+from pymongo import MongoClient
+
+conn_str = 'mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=rs0'
+client = MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+db = client.messagesApp
+
+
+
+
+
 
 
 

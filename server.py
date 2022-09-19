5cd379ac314efcb44a34371119df6fd2062a3b23
ori.ashkenazi <orish94@gmail.com>
2022-09-19 10:27:27 +0300

    adding connection Logi n and Register Using MongoDB and Posts

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 #Imports
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, session
+from MongoDB import db
 
 
 
@@ -16,7 +17,21 @@ def index():
     return render_template('index.html')
 
 
-app.route('/get_login', methods=['GET'])
+@app.route('/login', methods=['POST'])
+def login():
+    username: str = request.form["name"]
+    password: str = request.form["password"]
+    Uresult = db.users.find_one({'username': username})
+    Presult = db.users.find_one({'password': password})
+    if Uresult is not None and Presult is not None:
+        session['username'] = username
+        session['password'] = password
+        return redirect('/getallposts')
+    else:
+        return redirect('/get_register')
+
+
+@app.route('/get_login', methods=['GET'])
 def get_login():
     return redirect('/')
 
@@ -34,3 +49,19 @@ def register():
 @app.route('/get_register', methods=['GET'])
 def get_register():
     return render_template('register.html')
+
+
+
+@app.route('/getallposts', methods=['GET'])
+def getallposts():
+    collection = db.posts.find()
+    return render_template("chat.html", collection= collection,username=session['username'])
+
+@app.route('/post', methods=['POST'])
+def post():
+    post = {
+        "username": session['username'],
+        "post": request.form["post"]
+    }
+    db.posts.insert_one(post)
+    return redirect('/getallposts')
